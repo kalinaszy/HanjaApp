d0c142141f4e4aaac91d2a33ca5b377074ba7138
Kalina <kalina.szymczyk@gmail.com>
2019-07-09 20:44:23 +0200

    Change settings placement

--- myproject/myproject/settings/settings.py
+++ myproject/myproject/settings/settings.py
@@ -137,18 +137,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../../static'),
 )
 
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import settings
-from django.contrib.staticfiles.urls import static
+from myproject.settings import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from game.views import IndexView, PlayGameView, BestScoresView, CheckAnswersView, ComposeCommentView, CommentsView, \
